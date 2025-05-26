a05f6603abecd3d5e716de45bfbb179df6b12560
camila0424 <milabs.esp24@gmail.com>
2025-05-26 15:16:30 +0200

    Deployed new version of the page to production

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,2 +1,2 @@
-console.log(">> Ready :)");
+const t=document.querySelector(".js_hamburguer"),n=document.querySelector(".js_menu_ham");t.addEventListener("click",e=>{e.preventDefault(),n.classList.toggle("collapsed")});console.log("funcion toggle2");
 //# sourceMappingURL=main.js.map
